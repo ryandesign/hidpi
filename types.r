623bc5ff432aeb0a03d74c7dd30f1850d8811ead
Ryan Carsten Schmidt <git@ryandesign.com>
2026-04-17 23:54:33 -0500

    Add comment explaining mstr resource type

--- types.r
+++ types.r
@@ -3,6 +3,7 @@ SPDX-FileCopyrightText: © 2026 Ryan Carsten Schmidt <https://github.com/ryandes
 SPDX-License-Identifier: MIT
 */
 
+/* Technote OV 17: MultiFinder Revisited: The 6.0 System */
 type 'mstr' as 'STR ';
 
 type 'TEXT' {
