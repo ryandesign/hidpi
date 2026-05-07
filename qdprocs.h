bfe357d461c5431769f1696c3b0f280de25d5c7f
Ryan Carsten Schmidt <git@ryandesign.com>
2026-05-07 00:42:35 -0500

    Add project name to include guard

--- qdprocs.h
+++ qdprocs.h
@@ -3,8 +3,8 @@ SPDX-FileCopyrightText: © 2026 Ryan Carsten Schmidt <https://github.com/ryandes
 SPDX-License-Identifier: MIT
 */
 
-#ifndef QDPROCS_H
-#define QDPROCS_H
+#ifndef HIDPI_QDPROCS_H
+#define HIDPI_QDPROCS_H
 
 extern void set_port_2x(GrafPtr port);
 extern void init_qdprocs(void);
