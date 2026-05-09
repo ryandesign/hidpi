21a1b44949b02eb7431f403d29479295b5a04b36
Ryan Carsten Schmidt <git@ryandesign.com>
2026-05-09 01:42:41 -0500

    Add debug functions for various kinds of objects

--- debug.c
+++ debug.c
@@ -8,6 +8,290 @@ SPDX-License-Identifier: MIT
 #define k_debug_event_width 128
 #define k_line_height 16
 
+// pstrlcat is a Pascal version of strlcat, which appends one string to another.
+// src and dst are Pascal strings. size is the size of dst in bytes including
+// the length byte. pstrlcat will append at most size - dst[0] - 1 bytes, and
+// also caps the final string length at 255 characters.
+static Size pstrlcat(StringPtr dst, StringPtr src, Size size) {
+	Size intended_len;
+	Size max_len;
+	Size actual_len;
+	int src_index = 0;
+	int dst_index;
+
+	intended_len = src[0] + dst[0];
+
+	max_len = size - 1;
+	if (max_len > 255) {
+		max_len = 255;
+	}
+
+	if (intended_len > max_len) {
+		actual_len = max_len;
+	} else {
+		actual_len = intended_len;
+	}
+
+	dst_index = dst[0];
+	while (dst_index < actual_len) {
+		dst[++dst_index] = src[++src_index];
+	}
+	dst[0] = actual_len;
+
+	return intended_len;
+}
+
+// TODO: this all ends up being rather verbose, doesn't it? simplify with
+//       dedicated functions that append different types to a string
+
+static StringPtr num_to_str(long num, StringPtr dst, Size size) {
+	Str15 tmp;
+
+	dst[0] = 0;
+
+	NumToString(num, tmp);
+	pstrlcat(dst, tmp, size);
+
+	return dst;
+}
+
+static StringPtr pt_to_str(Point pt, StringPtr dst, Size size) {
+	Str15 tmp;
+
+	dst[0] = tmp[0] = 0;
+
+	pstrlcat(dst, "\p(", size);
+	pstrlcat(dst, num_to_str(pt.h, tmp, 16), size);
+	pstrlcat(dst, "\p,", size);
+	pstrlcat(dst, num_to_str(pt.v, tmp, 16), size);
+	pstrlcat(dst, "\p)", size);
+
+	return dst;
+}
+
+static StringPtr rect_to_str(Rect *rect, StringPtr dst, Size size) {
+	Str15 tmp;
+
+	dst[0] = tmp[0] = 0;
+
+	pstrlcat(dst, "\p(", size);
+	pstrlcat(dst, num_to_str(rect->left, tmp, 16), size);
+	pstrlcat(dst, "\p,", size);
+	pstrlcat(dst, num_to_str(rect->top, tmp, 16), size);
+	pstrlcat(dst, "\p,", size);
+	pstrlcat(dst, num_to_str(rect->right, tmp, 16), size);
+	pstrlcat(dst, "\p,", size);
+	pstrlcat(dst, num_to_str(rect->bottom, tmp, 16), size);
+	pstrlcat(dst, "\p)", size);
+
+	return dst;
+}
+
+static StringPtr numer_denom_to_str(Point numer, Point denom, StringPtr dst, Size size) {
+	Str15 tmp;
+
+	dst[0] = tmp[0] = 0;
+
+	pstrlcat(dst, "\p(", size);
+	pstrlcat(dst, num_to_str(numer.h, tmp, 16), size);
+	pstrlcat(dst, "\p/", size);
+	pstrlcat(dst, num_to_str(denom.h, tmp, 16), size);
+	pstrlcat(dst, "\p,", size);
+	pstrlcat(dst, num_to_str(numer.v, tmp, 16), size);
+	pstrlcat(dst, "\p/", size);
+	pstrlcat(dst, num_to_str(denom.v, tmp, 16), size);
+	pstrlcat(dst, "\p)", size);
+
+	return dst;
+}
+
+static StringPtr font_info_to_str(FontInfo *info, StringPtr dst, Size size) {
+	Str63 tmp;
+
+	dst[0] = tmp[0] = 0;
+
+	pstrlcat(dst, "\p[asc:", size);
+	pstrlcat(dst, num_to_str(info->ascent, tmp, 64), size);
+	pstrlcat(dst, "\p,dec:", size);
+	pstrlcat(dst, num_to_str(info->descent, tmp, 64), size);
+	pstrlcat(dst, "\p,widMax:", size);
+	pstrlcat(dst, num_to_str(info->widMax, tmp, 64), size);
+	pstrlcat(dst, "\p,lead:", size);
+	pstrlcat(dst, num_to_str(info->leading, tmp, 64), size);
+	pstrlcat(dst, "\p]", size);
+
+	return dst;
+}
+
+void debug_font_info(StringPtr message, FontInfo *info) {
+	Str255 dst;
+	Str63 tmp;
+
+	dst[0] = tmp[0] = 0;
+
+	pstrlcat(dst, message, 256);
+	pstrlcat(dst, font_info_to_str(info, tmp, 64), 256);
+
+	DebugStr(dst);
+}
+
+void debug_numer_denom(StringPtr message, Point numer, Point denom) {
+	Str255 dst;
+	Str31 tmp;
+
+	dst[0] = tmp[0] = 0;
+
+	pstrlcat(dst, message, 256);
+	pstrlcat(dst, numer_denom_to_str(numer, denom, tmp, 32), 256);
+
+	DebugStr(dst);
+}
+
+static unsigned char count_digits(unsigned long num, char base) {
+	unsigned char len = 0;
+
+	do {
+		++len;
+		num /= base;
+	} while (num > 0);
+
+	return len;
+}
+
+// TODO: add and respect size
+static StringPtr num_to_hex_str(unsigned long num, StringPtr dst, unsigned char len) {
+	unsigned char hex;
+
+	if (0 == len) {
+		len = count_digits(num, 0x10);
+	}
+	dst[0] = len;
+
+	while (len > 0) {
+		hex = num % 0x10;
+		hex += hex >= 0xA ? ('A' - 0xA) : '0';
+		dst[len--] = hex;
+		num /= 0x10;
+	}
+
+	return dst;
+}
+
+// TODO: respect size
+static StringPtr ptr_to_str(Ptr ptr, StringPtr str, Size size) {
+	return num_to_hex_str((unsigned long)StripAddress(ptr), str, 2 * sizeof(Ptr));
+}
+
+void debug_ptr(StringPtr message, Ptr ptr) {
+	Str255 dst;
+	Str15 tmp;
+
+	dst[0] = tmp[0] = 0;
+
+	pstrlcat(dst, message, 256);
+	pstrlcat(dst, ptr_to_str(ptr, tmp, 16), 256);
+
+	DebugStr(dst);
+}
+
+void debug_hex(StringPtr message, unsigned long hex) {
+	Str255 dst;
+	Str15 tmp;
+
+	dst[0] = tmp[0] = 0;
+
+	pstrlcat(dst, message, 256);
+	pstrlcat(dst, num_to_hex_str(hex, tmp, 0), 256);
+
+	DebugStr(dst);
+}
+
+void debug_num(StringPtr message, long num) {
+	Str255 dst;
+	Str15 tmp;
+
+	dst[0] = tmp[0] = 0;
+
+	pstrlcat(dst, message, 256);
+	pstrlcat(dst, num_to_str(num, tmp, 16), 256);
+
+	DebugStr(dst);
+}
+
+void debug_rect(StringPtr message, Rect *rect) {
+	Str255 dst;
+	Str31 tmp;
+
+	dst[0] = tmp[0] = 0;
+
+	pstrlcat(dst, message, 256);
+	pstrlcat(dst, rect_to_str(rect, tmp, 32), 256);
+
+	DebugStr(dst);
+}
+
+Boolean equal_buf(short count1, Ptr buf1, short count2, Ptr buf2) {
+	short i;
+
+	if (count1 != count2) {
+		return false;
+	}
+
+	for (i = 0; i < count1; ++i) {
+		if (buf1[i] != buf2[i]) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+void debug_buf(StringPtr message, short size, Ptr buf) {
+	Str255 dst;
+	short i = 0;
+	unsigned char c;
+
+	if (size > 255) {
+		size = 255;
+	}
+	dst[0] = size;
+
+	while (i < size) {
+		c = buf[i];
+		dst[++i] = c;
+	}
+
+	DebugStr(dst);
+}
+
+void debug_bits(StringPtr message, BitMap *src_bits, Rect *src_rect, Boolean src_hi, BitMap *dst_bits, Rect *dst_rect, Boolean dst_hi, RgnHandle mask_rgn) {
+	Str255 dst;
+	Str31 tmp;
+
+	dst[0] = tmp[0] = 0;
+
+	pstrlcat(dst, message, 256);
+	pstrlcat(dst, ptr_to_str(src_bits->baseAddr, tmp, 32), 256);
+	pstrlcat(dst, rect_to_str(src_rect, tmp, 32), 256);
+	if (src_hi) {
+		pstrlcat(dst, "\p@2x", 256);
+	}
+	pstrlcat(dst, "\p->", 256);
+	pstrlcat(dst, ptr_to_str(dst_bits->baseAddr, tmp, 32), 256);
+	pstrlcat(dst, rect_to_str(dst_rect, tmp, 32), 256);
+	if (dst_hi) {
+		pstrlcat(dst, "\p@2x", 256);
+	}
+	if (nil == mask_rgn) {
+		pstrlcat(dst, "\p nil", 256);
+	} else {
+		pstrlcat(dst, rect_to_str(&(**mask_rgn).rgnBBox, tmp, 32), 256);
+	}
+
+	DebugStr(dst);
+}
+
+// TODO: rewrite by concatenating using above functions and drawing string once
 void print_event(EventRecord *event) {
 	GrafPtr saved_port;
 	GrafPtr port;
@@ -110,3 +394,5 @@ void redraw_screen(void) {
 	HiliteMenu(0);
 	DrawMenuBar();
 }
+
+// TODO: use Size in more places?
--- debug.h
+++ debug.h
@@ -6,6 +6,15 @@ SPDX-License-Identifier: MIT
 #ifndef HIDPI_DEBUG_H
 #define HIDPI_DEBUG_H
 
+void debug_numer_denom(StringPtr message, Point numer, Point denom);
+void debug_font_info(StringPtr message, FontInfo *info);
+void debug_ptr(StringPtr message, Ptr ptr);
+void debug_hex(StringPtr message, unsigned long hex);
+void debug_num(StringPtr message, long num);
+void debug_rect(StringPtr message, Rect *rect);
+Boolean equal_buf(short count1, Ptr buf1, short count2, Ptr buf2);
+void debug_buf(StringPtr message, short size, Ptr buf);
+void debug_bits(StringPtr message, BitMap *src_bits, Rect *src_rect, Boolean src_hi, BitMap *dst_bits, Rect *dst_rect, Boolean dst_hi, RgnHandle mask_rgn);
 void print_event(EventRecord *event);
 void redraw_screen(void);
 
