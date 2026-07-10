c4a9a3147ce493893e032964e96d92e268d2e1b0
Ryan Carsten Schmidt <git@ryandesign.com>
2026-07-10 03:07:58 -0500

    No need to copy CrsrSave in init_data

--- init_data.c
+++ init_data.c
@@ -6,20 +6,18 @@ SPDX-License-Identifier: MIT
 #include "init_data.h"
 
 #include "macros.h"
-#include "missing_traps.h"
 
 Boolean init_data(data_t *data)
 {
 	CrsrBusy = true; // TODO: check if necessary
-	//enlarge_cursor();
+
 	{
 		Point hotspot = TheCrsr.hotSpot;
 
 		//pin_hotspot(&hotspot);
 		*(long *)&data->hotspot_2x = *(long *)&hotspot * k_scale;
 	}
-	// TODO: Color QuickDraw
-	BlockMoveData(CrsrSave, data->save_2x, sizeof(long) * rect_height(&CrsrRect));
+
 	CrsrBusy = false; // TODO: check if necessary
 
 	return true;
